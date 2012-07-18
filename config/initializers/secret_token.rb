f49163dc63d277529a29e98ecb8b33489b269fde
Kerolos Bebawy <kirolos@Kiro-VirtualBox.(none)>
2012-07-18 20:56:08 +0200

    created a new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+La2etlakMongo::Application.config.secret_token = '0c41e60c72e3f3d3b0c6f7be1fa4d57a692991d69267380197d2cc4edba0f7df2a54acd9e8172fdac246001d205658e87f87bf86208e7ba0e5d953aef12b1f15'
