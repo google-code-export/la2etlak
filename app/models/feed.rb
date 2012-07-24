5af84d2c208f44145b45c3e35e84dd1584cee4b1
mouaz <mouaz@mouaz.(none)>
2012-07-24 13:18:11 +0200

    ffeeedd

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -24,4 +24,30 @@ class Feed
 #interest _id should be there to create the RSS feed 
   validates :interest_id, :presence => true
 
+
+# author : Mouaz			 
+# get_feed is a method that takes a specific feed_id as an input  and searches the database 
+# for the feed with this id and returns #this feed to the caller
+
+  def self.get_feed(feed_id)
+      return Feed.find(feed_id)
+  end
+
+# A method that gets all the stories of a certain interest
+  def self.get_feeds_by_interest (interest_ido)
+    return Feed.where(interest_id: interest_ido).all.entries
+  end
+
+# A method that gets the feed by the interest_id
+def self.get_feed_by_interest (interest_ido)
+    return Feed.where(interest_id: interest_ido).first
+#return Feed.find_by_interest_id(interest_id)
+end
+# author : Mouaz			 
+# get_feed is a method that takes a specific feed_link as an input  and searches the database 
+# for the feed with this id and returns #this feed to the caller
+
+  def self.get_feed_by_link(feed_link)
+      return Feed.find_by(link: feed_link)
+  end
 end
