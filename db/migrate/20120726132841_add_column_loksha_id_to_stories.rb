83d321ddd923189dcf6a70ae10e202591302d873
gosgosakila <gosgosakila@gmail.com>
2012-07-27 13:33:00 +0200

    Deleted my migration

--- db/migrate/20120726132841_add_column_loksha_id_to_stories.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnLokshaIdToStories < ActiveRecord::Migration
-  def up
-    add_column :stories, :loksha_id, :string
-  end
-
-  def down
-    remove_column :stories, :loksha_id
-  end
-end
