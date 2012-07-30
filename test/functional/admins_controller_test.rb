2266735f6124c8d955400243476ee718d466fc9f
Diab <diab977@gmail.com>
2012-07-30 10:29:50 +0200

    Update issue 34 adding test

--- test/functional/admins_controller_test.rb
+++ test/functional/admins_controller_test.rb
@@ -301,4 +301,13 @@ class AdminsControllerTest < ActionController::TestCase
         assert_select "div[class=apple_pagination]"
       end
   end
+
+  #Author : Diab
+  test "admin has account red" do
+   #User.destroy_all
+   #Admin.destroy_all
+   post :create , :admin => {:email => "mailadmin@mail.com" , :password => "123456" , :password_confirmation => "123456" }#, :first_name=>"Admin Son" , :last_name => "Admin father"}
+   assert User.has_account(:email) , "email should be mailadmin@mail.com"
+  end
+  
 end
