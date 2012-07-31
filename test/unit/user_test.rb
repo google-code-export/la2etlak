b3c54fdbf07816a6a8089fb50ec08c847df6de7d
omar <omar@omar.(none)>
2012-07-31 17:07:47 +0200

    Update issue 43 updating tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -532,8 +532,8 @@ end
 	UserSession.create(ben)
         ahmed = users(:ahmed)
         UserSession.create(ahmed)
-        	     
-        assert_equal(Noitification.last.type,1,"friendship request sent notification entered database correctly")
+         	     
+        assert_equal(Noitification.last.notify_type,1,"friendship request sent notification entered database correctly")
         end
   
   test "friend request accepeted notification RED" do
@@ -542,7 +542,7 @@ end
         ahmed = users(:ahmed)
         UserSession.create(ahmed)
         
-        assert_equal(Noitification.last.type,2,"friendship accepted notification entered database correctly")  	
+        assert_equal(Noitification.last.notify_type,2,"friendship accepted notification entered database correctly")  	
   end  
 
   test "recommend story notification RED" do
@@ -551,7 +551,7 @@ end
         ahmed = users(:ahmed)
         UserSession.create(ahmed)
         
-        assert_equal(Noitification.last.type,3,"friend recommend a story notification entered database correctly")  	
+        assert_equal(Noitification.last.notify_type,3,"friend recommend a story notification entered database correctly")  	
 
   end
   
@@ -564,7 +564,8 @@ end
         story = Story.create(interest_id: interest.id , title:"story title" , media_link:"http://www.omar.com")
         comment1 = Comment.create(user_id: ben.id ,story_id: story.id , content:"1")
         comment2 = Comment.create(user_id: ahmed.id ,story_id: story.id , content:"2")
-        assert_equal(Noitification.last.type,4,"friend comment on story i commented on notification entered database correctly")  	
+        assert_equal(Noitification.last.notify_type,4,"friend comment on story i commented on notification entered database correctly")
+        assert_equal(Noitification.last.owner.ben.id,"notification not sent properly")  	
   
   end
   
@@ -577,8 +578,8 @@ end
         story = Story.create(interest_id: interest.id , title:"story title" , media_link:"http://www.omar.com")
         comment1 = Comment.create(user_id: ben.id ,story_id: story.id , content:"1")
         CommentUpDown.create(action: 1 , user_id: ben.id , comment_id: comment1.id)
-        assert_equal(Noitification.last.type,5,"friend like comment notification entered database correctly")  	
-  
+        assert_equal(Noitification.last.notify_type,5,"friend like comment notification entered database correctly")  	
+        assert_equal(Noitification.last.owner,ben.id,"notification not sent properly")   
   end    
 
   test "dislike your notification RED" do
